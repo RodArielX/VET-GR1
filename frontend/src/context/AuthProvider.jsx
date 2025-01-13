61576616ca5b1dc3f8cc323e2ca38f5e1f99a5a6
RodArielX <ariel.ashqui@epn.edu.ec>
2025-01-13 15:49:52 -0500

    Lunes 13-01

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -4,22 +4,22 @@ import { Children, createContext, useEffect, useState } from 'react'
 // Creación del grupo de WhatsApp (AuthContext)
 const AuthContext = createContext()
 
-    
-    
+
+
 
 // Crear el mensaje(AuthProvider) // Integrantes (children)
-const AuthProvider =({children})=>{
+const AuthProvider = ({ children }) => {
 
-    const[auth, setAuth] = useState({})
+    const [auth, setAuth] = useState({})
 
-    const perfil = async(token) => {
+    const perfil = async (token) => {
         try {
             const url = `${import.meta.env.VITE_BACKEND_URL}/perfil`
 
-            const options= {
-                headers:{
-                    'Content-Type':'application/json',
-                    Authorization:`Bearer ${token}`
+            const options = {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
                 }
 
             }
@@ -31,15 +31,34 @@ const AuthProvider =({children})=>{
             console.log(error)
 
         }
-      
+
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const token = localStorage.getItem('token')
-        if(token){
+        if (token) {
             perfil(token)
         }
     }, [])
+
+    const actualizarPerfil = async (datos) => {
+        const token = localStorage.getItem('token')
+        try {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const respuesta = await axios.put(url, datos, options)
+            perfil(token)
+            return { respuesta: respuesta.data.msg, tipo: true }
+        } catch (error) {
+            return { respuesta: error.response.data.msg, tipo: false }
+        }
+    }
 
 
 
@@ -47,16 +66,19 @@ const AuthProvider =({children})=>{
         {
             // Contenido del mensaje
             auth,
-            setAuth
+            setAuth,
+            actualizarPerfil
         }
     }>
         {children}
-        </AuthContext.Provider>
+    </AuthContext.Provider>
 }
 
 
 
-export{
+
+
+export {
     AuthProvider
 }
 export default AuthContext
